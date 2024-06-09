45105698407226270358f5637d45107efd35ebbc
RyzenMaster <nenaddamjanovic@hotmail.rs>
2024-06-09 17:02:00 +0200

    adding lines

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ for index, row in datafile.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(10, 21, 200, 21)
+    #  pdf.line(10, 21, 200, 21)
+    #  set all lines
+    for line in range(21, 290, 10):
+        pdf.line(10, line, 200, line)
 
     #  Set the futer
     pdf.ln(260)  # Mesto gde futer počinje
@@ -24,9 +27,12 @@ for index, row in datafile.iterrows():
     for i in range(row["Pages"] - 1):
         pdf.add_page()
         #  Set the futer
-        pdf.ln(270)  # Mesto gde futer počinje
+        pdf.ln(272)  # Mesto gde futer počinje
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        #  set all lines
+        for line in range(21, 290, 10):
+            pdf.line(10, line, 200, line)
 
 pdf.output("output.pdf")
