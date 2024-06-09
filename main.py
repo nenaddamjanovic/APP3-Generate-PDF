b9f8912b09d372b50ede640e47cb4cdc90b16857
RyzenMaster <nenaddamjanovic@hotmail.rs>
2024-06-09 16:37:21 +0200

    adding all pages

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 datafile = pd.read_csv("topics.csv")
 
 for index, row in datafile.iterrows():
-    pdf.add_page()
+    pdf.add_page()  # Kreira naslovnu stranicu
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
     pdf.line(10, 21, 200, 21)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()  # Kreira ostale prazne stranice (-1 prva stranica)
 
 pdf.output("output.pdf")
